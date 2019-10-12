ecfa794baca06399ede7ec9323a20df002fc3f31
Eric <ericvanhallen@gmail.com>
2019-10-12 13:47:46 -0400

    Scaffolded basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TitleBarComponent } from './title-bar/title-bar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FormComponent } from './form/form.component';
+import { FormPreviewComponent } from './form-preview/form-preview.component';
+import { FormCreateComponent } from './form-create/form-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleBarComponent,
+    DashboardComponent,
+    FormComponent,
+    FormPreviewComponent,
+    FormCreateComponent
   ],
   imports: [
     BrowserModule
