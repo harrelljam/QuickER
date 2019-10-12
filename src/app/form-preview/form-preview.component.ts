ef107013db62e0ad05d8e73fdff6aaae635649e4
Eric <ericvanhallen@gmail.com>
2019-10-12 18:48:07 -0400

    Linked components together

--- src/app/form-preview/form-preview.component.ts
+++ src/app/form-preview/form-preview.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Form } from '../models/form';
 
 @Component({
-  selector: 'app-form-preview',
+  selector: 'form-preview',
   templateUrl: './form-preview.component.html',
   styleUrls: ['./form-preview.component.css']
 })
 export class FormPreviewComponent implements OnInit {
+
+  @Input("data") data: Form
 
   constructor() { }
 
