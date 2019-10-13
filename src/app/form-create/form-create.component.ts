bfa3e4d793880ea119608f5a6abc58959d19656f
Eric <ericvanhallen@gmail.com>
2019-10-13 03:09:43 -0400

    Final Buildgit add .

--- src/app/form-create/form-create.component.ts
+++ src/app/form-create/form-create.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Form } from '../models/form';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'form-create',
@@ -10,9 +11,15 @@ export class FormCreateComponent implements OnInit {
 
   form = {} as Form;
 
-  constructor() { }
+  constructor(public db: AngularFirestore) { }
 
   ngOnInit() {
+  }
+
+  submit(){
+    this.db.collection("patients").add(this.form).then(()=>{
+      this.form = {} as Form;
+    });
   }
 
 }
